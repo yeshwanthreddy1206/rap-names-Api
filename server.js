3e7662f2a5a667ba99410b5083035a308b0f1ee6
yeshwanthreddy1206 <yeshwanthreddyy08@gmail.com>
2026-06-29 10:51:04 +0530

    This is firs repo of rap-names-Api

---
+++ server.js
@@ -0,0 +1,47 @@
+const express = require('express')
+const app = express()
+const PORT = 8000
+
+const rappers = {
+    'roll rida' :{
+        Age : 29,
+        BirthName : 'roll rida',
+        BirthLocation : 'Hyderabad, India'
+    },
+     'hanuman kind' :{
+        Age : 32,
+        BirthName : 'Sooraj Cherukat',
+        BirthLocation : 'Kondotty, Malappuram, Kerala, India'
+    },
+     'karan aujla' :{
+        Age : 32,
+        BirthName : 'Jaskaran Singh Aujla',
+        BirthLocation : 'Ludhiana district, Punjab, India'
+    },
+     'error' :{
+        Age : 'Error',
+        BirthName : 'Error',
+        BirthLocation : 'Error'
+    },
+ 
+}
+
+app.get('/', (request, response)=>{
+    response.sendFile(__dirname + '/index.html')
+})
+
+app.get('/api/:rapperName', (request, response)=>{
+    const rappersNames = request.params.rapperName.toLowerCase()
+    if(rappers[rappersNames]){
+        response.json(rappers[rappersNames])
+    }else{
+        response.json(rappers['error'])
+    }
+
+    // response.json(rappers)
+})
+
+app.listen( PORT, ()=>{
+    console.log(`The server is running on port ${PORT}, Go catch it! `)
+})
+
